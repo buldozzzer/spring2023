6dd868f26bd9de6841c2e35dccf4a83d51e6b2fc
buldozzzer <1and130699@gmail.com>
2023-11-13 19:26:43 +0300

    add controller

---
+++ src/main/java/com/example/spring2023/controller/AssigmentController.java
@@ -0,0 +1,54 @@
+package com.example.spring2023.controller;
+
+import com.example.spring2023.model.Task;
+import com.example.spring2023.model.User;
+import com.example.spring2023.service.TaskService;
+import com.example.spring2023.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class AssigmentController {
+    private UserService userService;
+    private TaskService taskService;
+
+    @Autowired
+    public AssigmentController(UserService userService, TaskService taskService) {
+        this.userService = userService;
+        this.taskService = taskService;
+    }
+
+    @GetMapping("/assignment")
+    public String showAssigmentForm(Model model) {
+        model.addAttribute("users", userService.findAll());
+        model.addAttribute("freeTasks", taskService.findFreeTasks());
+        return "forms/assignment";
+    }
+
+    @GetMapping("/assignment/{userId}")
+    public String showUserAssigmentForm(@PathVariable Long userId, Model model) {
+        model.addAttribute("selectedUser", userService.getUserById(userId));
+        model.addAttribute("users", userService.findAll());
+        model.addAttribute("freeTasks", taskService.findFreeTasks());
+        return "forms/assignment";
+    }
+
+    @GetMapping("/assignment/assign/{userId}/{taskId}")
+    public String assignTaskToUser(@PathVariable Long userId, @PathVariable Long taskId) {
+        Task selectedTask = taskService.getTaskById(taskId);
+        User selectedUser = userService.getUserById(userId);
+        taskService.assignTaskToUser(selectedTask, selectedUser);
+        return "redirect:/assignment/" + userId;
+    }
+
+    @GetMapping("assignment/unassign/{userId}/{taskId}")
+    public String unassignTaskFromUser(@PathVariable Long userId, @PathVariable Long taskId) {
+        Task selectedTask = taskService.getTaskById(taskId);
+        taskService.unassignTask(selectedTask);
+        return "redirect:/assignment/" + userId;
+    }
+
+}
